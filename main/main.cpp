d349ba1cd90c1c8a9f2744f35bb6846cd41bba90
JamesBMiddleton <james.ben.middleton@gmail.com>
2021-12-21 12:55:27 +0000

    works with basic branching without random next segments implemented

--- main/main.cpp
+++ main/main.cpp
@@ -8,23 +8,25 @@
 
 struct Coords {int x, y;};
 struct CoordsOffset {int x, y;};
-struct BranchCoordsOffsets {CoordsOffset left_branch, right_branch;};
-struct BranchNames {std::string left_branch, right_branch;};
+
+struct BranchInfo
+{   
+    std::string left_name, right_name;
+    CoordsOffset left_offset, right_offset;
+};
 
 struct SegmentInfo
+// Static class used to create and access segment types and their attributes.
 {
-    static void create_segment(std::string name, std::string ASCII,std::map<std::string, double> probabilities,
-                               std::map<std::string, CoordsOffset> next_segment_pos);
-    static void create_branching_segment(std::string name, std::string ASCII, std::map<std::string, double> probabilities, 
-                                         BranchCoordsOffsets relative_branch_pos, BranchNames branch_names);
+    static void create_segment_type(std::string name, std::string ASCII,std::map<std::string, double> probabilities,
+                                    std::map<std::string, CoordsOffset> next_segment_pos);
+    static void create_branching_segment_type(std::string name, std::string ASCII, BranchInfo branch_info);
 
-    static std::string get_ASCII(std::string name) {return segment_ASCIIs[name];}
-    static std::map<std::string, double> get_probabilities(std::string name) {return next_segment_probabilities[name];}
-    static std::map<std::string, CoordsOffset> get_next_segment_coords(std::string name) {return next_segment_positions[name];}
-    static bool is_branching(std::string name) {return branch_bools[name];}
-    static BranchCoordsOffsets get_relative_branch_coords(std::string name) {return branching_segment_branch_coords[name];}
-    static BranchNames get_branch_names(std::string name) {return branching_segment_branch_names[name];}
-
+    static std::string get_ASCII(std::string name) {return segment_ASCIIs.at(name);}
+    static std::map<std::string, double> get_probabilities(std::string name) {return next_segment_probabilities.at(name);}
+    static std::map<std::string, CoordsOffset> get_next_segment_coords(std::string name) {return next_segment_positions.at(name);}
+    static bool is_branching(std::string name) {return branch_bools.at(name);}
+    static BranchInfo get_branch_info(std::string name) {return branch_data.at(name);}
 private:
     SegmentInfo() {};
 
@@ -32,20 +34,20 @@ private:
     static std::map<std::string, std::string> segment_ASCIIs;
     static std::map<std::string, std::map<std::string, double>> next_segment_probabilities;
     static std::map<std::string, std::map<std::string, CoordsOffset>> next_segment_positions;
+
     static std::map<std::string, bool> branch_bools;
-    static std::map<std::string, BranchCoordsOffsets> branching_segment_branch_coords;
-    static std::map<std::string, BranchNames> branching_segment_branch_names;
+    static std::map<std::string, BranchInfo> branch_data;
 };
 std::vector<std::string> SegmentInfo::segment_names;
 std::map<std::string, std::string> SegmentInfo::segment_ASCIIs;
 std::map<std::string, std::map<std::string, double>> SegmentInfo::next_segment_probabilities;
 std::map<std::string, std::map<std::string, CoordsOffset>> SegmentInfo::next_segment_positions;
-std::map<std::string, bool> SegmentInfo::branch_bools;
-std::map<std::string, BranchCoordsOffsets> SegmentInfo::branching_segment_branch_coords;
-std::map<std::string, BranchNames> SegmentInfo::branching_segment_branch_names;
 
-void SegmentInfo::create_segment(std::string name,std::string ASCII, std::map<std::string, double> probabilities,
-                                 std::map<std::string, CoordsOffset> next_segment_pos)
+std::map<std::string, bool> SegmentInfo::branch_bools;
+std::map<std::string, BranchInfo> SegmentInfo::branch_data;
+
+void SegmentInfo::create_segment_type(std::string name,std::string ASCII, std::map<std::string, double> probabilities,
+                                      std::map<std::string, CoordsOffset> next_segment_pos)
 {
     segment_names.push_back(name);
     segment_ASCIIs.insert({name, ASCII});
@@ -54,40 +56,43 @@ void SegmentInfo::create_segment(std::string name,std::string ASCII, std::map<st
     next_segment_positions.insert({name, next_segment_pos});
 }
 
-void SegmentInfo::create_branching_segment(std::string name, std::string ASCII, std::map<std::string, double> probabilities, 
-                                           BranchCoordsOffsets relative_branch_pos, BranchNames branch_names)
+void SegmentInfo::create_branching_segment_type(std::string name, std::string ASCII, BranchInfo branch_info)
 {
     segment_names.push_back(name);
     segment_ASCIIs.insert({name, ASCII});
     branch_bools.insert({name, true});
-    branching_segment_branch_coords.insert({name, relative_branch_pos});
+    branch_data.insert({name, branch_info});
 }
 
 struct Segment
+// Designed for instancing individual segments.
+// Access SegmentInfo members using 'type' for a segment's predefined attributes.
 {
-    Segment(std::string n, Coords c, bool t=false, bool b=false) :type{n}, coords{c}, is_terminator{t}, is_branching{b} {}
+    Segment(std::string seg_type, Coords seg_coords, bool seg_terminates=false) :type{seg_type}, coords{seg_coords}, is_terminator{seg_terminates} {}
     std::string type;
     Coords coords;
     bool is_terminator;
-    bool is_branching;
 };
 
 void clear_screen(int x, int y)
-// Prints a string of whitespace to clear the screen.
+// Prints a string of whitespace to clear the screen and removes the cursor.
 // This should probably take into account the terminal col number in the future.
 {
     int total{x*y};
     std::string s(total, ' ');
-    std::cout << s;
+    std::cout << s << "\x1b[?25l";
 }
 
 std::vector<std::vector<char>> create_canvas(int x, int y)
+// Creates a 2D vector of whitespace chars.
 {
     std::vector<std::vector<char>> canvas(x, std::vector<char>(y, ' '));
     return canvas;
 }
 
-void print_to_screen(Segment segment)
+void print_segment(Segment segment)
+// Uses the coordinates and type members of a Segment object.
+// Accesses SegmentInfo to retrieve the ASCII representation of a segment.
 {
     std::cout << "\x1B[" << segment.coords.x << ';' << segment.coords.y << 'H';
     std::cout << SegmentInfo::get_ASCII(segment.type);
@@ -95,21 +100,80 @@ void print_to_screen(Segment segment)
 }
 
 void initialize_segment_types()
+// SegmentInfo starts empty, each segment type is created here.
 {
-    SegmentInfo::create_segment("trunk_right",
+    SegmentInfo::create_segment_type("trunk_right",
                             "/   /",
                             std::map<std::string, double> {{"trunk_right", 1}},
-                            std::map<std::string, CoordsOffset> {{"trunk_right", {1, 0}}});
-    SegmentInfo::create_segment("base",
+                            std::map<std::string, CoordsOffset> {{"trunk_right", {-1, 1}}, {"new_arm_left", {-1, 0}}});
+    SegmentInfo::create_segment_type("trunk_base",
                                 "/     \\",
                                 std::map<std::string, double> {{"trunk_right", 1}},
-                                std::map<std::string, CoordsOffset> {{"trunk_right", {1, 0}}});
+                                std::map<std::string, CoordsOffset> {{"trunk_right", {-1, 1}}});
+    SegmentInfo::create_segment_type("arm_left",
+                                 "\\ \\",
+                                std::map<std::string, double> {{"arm_left", 1}},
+                                std::map<std::string, CoordsOffset> {{"arm_left", {-1, -1}}});
+    SegmentInfo::create_branching_segment_type("new_arm_left",
+                                 "\\    /",
+                                 BranchInfo{"arm_left", "trunk_right", {-1,-1}, {-1, 2}});
 }
 
 Segment initialise_tree(std::vector<std::vector<char>> canvas)
 {
-    Segment base{"base", {24, 40}};
+    Segment base{"trunk_base", {24, 30}};
+    print_segment(base);
     return base;
+}
+
+Segment pick_next_segment(Segment previous_segment, std::vector<std::vector<char>> canvas, int i)
+{   
+    std::string next_seg_name;  // Static for now.
+    if (i==1) next_seg_name = "trunk_right";
+    if (i==2) next_seg_name = "new_arm_left";
+
+
+    CoordsOffset offset{SegmentInfo::get_next_segment_coords(previous_segment.type).at(next_seg_name)};
+    Coords next_seg_coords{previous_segment.coords.x + offset.x, previous_segment.coords.y + offset.y};
+    Segment next_segment{next_seg_name, next_seg_coords};
+    return next_segment;
+}
+
+void add_segment(Segment previous_segment, std::vector<std::vector<char>> canvas)
+{
+    int i{1}; // for testing.
+    while (i<3)
+    {
+        Segment next_segment = pick_next_segment(previous_segment, canvas, i);
+        // add_to_canvas(next_segment, canvas)
+        print_segment(next_segment);
+        if (next_segment.is_terminator)
+            break;
+        if (SegmentInfo::is_branching(next_segment.type))
+        {
+            BranchInfo branch_info = SegmentInfo::get_branch_info(next_segment.type);
+
+            Coords left_branch_coords{next_segment.coords.x + branch_info.left_offset.x, next_segment.coords.y + branch_info.left_offset.y};
+            std::string left_branch_type{branch_info.left_name};
+            Segment left_branch{left_branch_type, left_branch_coords};
+            std::this_thread::sleep_for(std::chrono::milliseconds(500));
+            print_segment(left_branch);
+            // add_to_canvas(left_branch);
+            // add_segment(left_branch, canvas);  // Recurse.
+
+            Coords right_branch_coords{next_segment.coords.x + branch_info.right_offset.x, next_segment.coords.y + branch_info.right_offset.y};
+            std::string right_branch_type{branch_info.right_name};
+            Segment right_branch{right_branch_type, right_branch_coords};
+            Segment previous_segment = right_branch;
+            std::this_thread::sleep_for(std::chrono::milliseconds(500));
+            print_segment(right_branch);
+            // add_to_canvas(right_branch);
+        }
+        else
+            previous_segment = next_segment;
+        std::this_thread::sleep_for(std::chrono::milliseconds(500));
+        i++;
+    }
 }
 
 int main()
@@ -118,7 +182,7 @@ int main()
     clear_screen(24, 80);
     std::vector<std::vector<char>> canvas = create_canvas(24, 80);
     Segment base = initialise_tree(canvas);
-    print_to_screen(base);
-    //std::cout << "\x1B[" << 24 << ";" << 1 << "H";
+    add_segment(base, canvas);
+    //std::cout << "\x1B[" << 24 << ";" << 1 << "H";  // there's something weird happening with cout buffering/flushing.
     return 0;
 }
